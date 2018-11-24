a63b93da4a8bb212f053375dcc4cd1b46b04b345
Thomas Lorntzen <cyrthomas1@gmail.com>
2018-11-24 15:19:22 +0100

    Add eslint rules

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static('dist'));
+app.use(express.static("dist"));
 
 app.listen(port, () => {
     console.log("Server running at port: " + port);
